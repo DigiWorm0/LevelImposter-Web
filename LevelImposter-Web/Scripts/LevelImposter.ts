16353302468c318ca3eb416ded881da0bf8bc8de
Austin <aus343@gmail.com>
2021-04-08 16:55:17 -0500

    Various Bug Fixes

---
+++ LevelImposter-Web/Scripts/LevelImposter.ts
@@ -0,0 +1,30 @@
+﻿import { Graphics } from './graphics/GraphicsHandler.js'
+import { InputHandler } from './input/InputHandler.js';
+import { ItemDB } from './map/ItemDB.js';
+import { MapHandler } from './map/MapHandler.js';
+
+class LevelImposter {
+	g: Graphics;
+	i: InputHandler;
+	m: MapHandler;
+
+	constructor() {
+		this.i = new InputHandler();
+		this.g = new Graphics();
+		this.m = new MapHandler(this.g.canvas.objRender.cam);
+
+		/*
+		Object.keys(ItemDB.Tasks).forEach((key, index) => {
+			var i = MapHandler.addExisting(key, key);
+			MapHandler.map.objs[i].x = index * 2;
+			MapHandler.map.objs[i].y = 0;
+			MapHandler.isAdding = false;
+		});*/
+	}
+};
+
+// Init
+let li: LevelImposter;
+$(function () {
+	li = new LevelImposter();
+});
