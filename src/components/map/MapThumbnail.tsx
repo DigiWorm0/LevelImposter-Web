bde40f917e3e9f7d4b2052eafe6c09977e4fe740
Digi <aus343@gmail.com>
2022-08-16 16:23:24 -0500

    Thumbnail Bug Fix

--- src/components/map/MapThumbnail.tsx
+++ src/components/map/MapThumbnail.tsx
@@ -1,23 +1,25 @@
-import { Button } from "react-bootstrap";
-import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import LIMetadata from "../../types/LIMetadata";
 import MapTags from "./MapTags";
 
 export default function MapThumbnail(props: { map: LIMetadata }) {
     const map = props.map;
-    const navigate = useNavigate();
 
     return (
-        <Button
-            onClick={() => navigate(`/map/${map.id}`)}
-            className="list-group-item list-group-item-action">
+        <Link to={`/map/${map.id}`} className="list-group-item list-group-item-action">
             <MapTags
                 isPublic={map.isPublic}
                 isVerified={map.isVerified}
             />
             <h4>{map.name}</h4>
             <h5>by {map.authorName}</h5>
-            <p>{map.description === "" ? <i>No Description</i> : map.description}</p>
-        </Button>
+            <p style={{
+                whiteSpace: "nowrap",
+                overflow: "hidden",
+                textOverflow: "ellipsis"
+            }}>
+                {map.description === "" ? <i>No Description</i> : map.description}
+            </p>
+        </Link>
     );
 }
