22069f5d726902fed2140a2109cf38cef44262f8
Digi <aus343@gmail.com>
2022-08-27 05:55:53 -0500

    Default Thumbnails

--- src/components/map/MapThumbnail.tsx
+++ src/components/map/MapThumbnail.tsx
@@ -1,29 +1,24 @@
 import { Link } from "react-router-dom";
 import { useThumbnail } from "../../hooks/useMaps";
 import LIMetadata from "../../types/LIMetadata";
-import MapTags from "./MapTags";
 
 export default function MapThumbnail(props: { map: LIMetadata }) {
     const thumbnail = useThumbnail(props.map.authorID, props.map.id);
     const map = props.map;
+
+    const thumbnailURL = thumbnail ? thumbnail : "/DefaultThumbnail.png";
 
     return (
         <Link
             to={`/map/${map.id}`}
             className={"list-group-item list-group-item-action" + (map.isPublic ? "" : " list-group-item-danger")}>
 
-            {thumbnail && (
-                <img
-                    src={thumbnail}
-                    alt={map.name}
-                    style={{ width: 412, height: 144, objectFit: "cover", margin: 10, borderRadius: 5 }}
-                />
-            )}
-
-            <MapTags
-                isPublic={map.isPublic}
-                isVerified={map.isVerified}
+            <img
+                src={thumbnailURL}
+                alt={"Thumbnail for " + map.name}
+                style={{ width: 412, height: 144, objectFit: "cover", margin: 10, borderRadius: 5, marginBottom: 20 }}
             />
+
             <h4>{map.name}</h4>
             <h5>by {map.authorName}</h5>
             <p>
--- src/screens/Maps.tsx
+++ src/screens/Maps.tsx
@@ -2,7 +2,6 @@ import { Col, Container, ListGroup, Row } from 'react-bootstrap';
 import { Clock, HeartFill, StarFill } from 'react-bootstrap-icons';
 import BetaHeader from '../components/home/BetaHeader';
 import MainHeader from '../components/MainHeader';
-import MapBanner from '../components/map/MapBanner';
 import MapThumbnail from '../components/map/MapThumbnail';
 import { useRecentMaps, useTopMaps, useVerifiedMaps } from '../hooks/useMaps';
 
@@ -22,7 +21,7 @@ export default function Maps() {
                             <StarFill color='gold' size={26} style={{ marginRight: 8, marginBottom: 5 }} />
                             Featured Maps
                         </h3>
-                        <ListGroup horizontal>
+                        <ListGroup horizontal style={{ overflowX: "scroll" }}>
                             {featuredMaps.map((map, index) => (
                                 <>
                                     <MapThumbnail
@@ -35,32 +34,43 @@ export default function Maps() {
                         </ListGroup>
                     </Col>
                 </Row>
+
                 <Row>
-                    <Col sm={7}>
-                        <h3 style={{ textAlign: "center", marginTop: 15 }}>
+                    <Col>
+                        <h3 style={{ textAlign: "center", marginTop: 20 }}>
                             <HeartFill color='red' size={26} style={{ marginRight: 8, marginBottom: 5 }} />
                             Most Liked
                         </h3>
-                        <ListGroup>
-                            {topMaps.map((map) => (
-                                <MapBanner
-                                    key={map.id}
-                                    map={map}
-                                />
+                        <ListGroup horizontal style={{ overflowX: "scroll" }}>
+                            {topMaps.map((map, index) => (
+                                <>
+                                    <MapThumbnail
+                                        key={map.id}
+                                        map={map}
+                                    />
+                                    {index % 2 === 1 && <br />}
+                                </>
                             ))}
                         </ListGroup>
                     </Col>
-                    <Col sm={5}>
-                        <h3 style={{ textAlign: "center", marginTop: 15 }}>
+                </Row>
+
+
+                <Row>
+                    <Col>
+                        <h3 style={{ textAlign: "center", marginTop: 20 }}>
                             <Clock color='#0d6efd' size={26} style={{ marginRight: 8, marginBottom: 5 }} />
                             Recent
                         </h3>
-                        <ListGroup>
-                            {recentMaps.map((map) => (
-                                <MapBanner
-                                    key={map.id}
-                                    map={map}
-                                />
+                        <ListGroup horizontal style={{ overflowX: "scroll" }}>
+                            {recentMaps.map((map, index) => (
+                                <>
+                                    <MapThumbnail
+                                        key={map.id}
+                                        map={map}
+                                    />
+                                    {index % 2 === 1 && <br />}
+                                </>
                             ))}
                         </ListGroup>
                     </Col>
