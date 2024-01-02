9ab011ebf56c07e8400afcf7c96bda9f226b8a2f
Digi <aus343@gmail.com>
2024-01-02 05:48:05 -0600

    Port to Vite & Upgrade Packages

--- src/components/home/KoFi.tsx
+++ src/components/home/KoFi.tsx
@@ -1,13 +1,7 @@
-import { KofiButton } from "react-kofi-button";
 
 export default function KoFi() {
     return (
-        <KofiButton
-            username="DigiWorm"
-            label="Sponsor me on Ko-fi"
-            preset="skinny"
-            backgroundColor="#FF5E5B"
-            animation="on_hover"
-        />
+        <>
+        </>
     );
 }
